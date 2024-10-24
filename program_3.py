35b2106b778f73eaec4403230d51a6afaeb4e3f1
The-R0nin <2025esteele@isd2144.org>
2024-10-24 14:24:38 -0500

    Update program_3.py
    Ethan Steele
    10/24
    Created base, working on logic

--- program_3.py
+++ program_3.py
@@ -6,3 +6,29 @@
 # The program should count of the number of correct and incorrect responses.  
 # (You could alternatively use another country and provinces, 
 # or countries of the world and capitals).
+import random
+American_States = {'Alabama':'Montgomery', 'Alaska':'Juneau', 'Arizona':'Phoenix', 
+                   'Arkansas':'Little Rock', 'California':'Sacramento', 'Colorado':'Denver', 
+                   'Conneticut':'Hartford', 'Delaware':'Dover', 'Florida':'Tallahassee', 
+                   'Georgia':'Atlanta', 'Hawaii':'Honolulu', 'Idaho':'Boise', 'Illinois':'Springfeild', 
+                   'Indiana':'Indianapolis', 'Iowa':'Des Moines', 'Kansas':'Topeka', 
+                   'Kentucky':'Frankfort', 'Louisiana':'Baton Rouge', 'Maine':'Augusta', 
+                   'Maryland':'Annapolis', 'Massachusetts':'Boston', 'Michigan':'Lansing', 
+                   'Minnesota':'Saint Paul', 'Mississippi':'Jackson', 'Missouri':'Jefferson City', 
+                   'Montana':'Helena', 'Nebraska':'Lincoln', 'Nevada':'Carson City', 
+                   'New Hampshire':'Concord', 'New Jersey':'Trenton', 'New Mexico':'Santa Fe', 
+                   'New York':'Albany', 'North Carolina':'Raleigh', 'North Dakota':'Bismarck', 
+                   'Ohio':'Columbus', 'Oklahoma':'Oklahoma City', 'Oregon':'Salem', 
+                   'Pennsyvania':'Harrisburg', 'Rhode Island':'Providence', 'South Carolina':'Columbia', 
+                   'South Dakota':'Pierre', 'Tennessee':'Nashville', 'Texas':'Austin', 
+                   'Utah':'Salt Lake City', 'Vermont':'Montpelier', 'Virginia':'Richmond', 
+                   'Washington':'Olympia', 'West Virginia':'Charleston', 'Wisconsin':'Madison', 
+                   'Wyoming':'Cheyenne',}
+def state_quiz():
+    i = random.randint[0,50]
+    question = input('What is the captial of {American_States[i]}')
+    if question in American_States[i] is True:
+        print('correct')
+    else:
+        print('Incorrect, The Answer is {American_States[i]}')
+    state_quiz()
